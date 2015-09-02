3f5ef584f3b218c0d0a997feddd6df37e0119a36
Ditesh Kumar <ditesh.kumar@snapdeal.com>
2015-09-02 09:13:00 +0530

    java webapp skeleton commit

---
+++ web/src/main/java/com/snapdeal/pears/whatsapp3c/controller/AppController.java
@@ -0,0 +1,20 @@
+package com.snapdeal.pears.whatsapp3c.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by ditesh on 2/9/15.
+ */
+@Controller
+public class AppController {
+
+    @RequestMapping(value = "/healthcheck", method = RequestMethod.GET, produces = "text/html")
+    public @ResponseBody
+    String createRequestPage() {
+        return "Health OK";
+    }
+    
+}
---
+++ web/src/main/java/com/snapdeal/pears/whatsapp3c/service/MessageService.java
@@ -0,0 +1,10 @@
+package com.snapdeal.pears.whatsapp3c.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by ditesh on 2/9/15.
+ */
+@Service
+public class MessageService {
+}
