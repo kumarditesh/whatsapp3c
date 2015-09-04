0fc0013f7e401778bf349777d49fb4b792e58736
Loki <lokesh.chhaparwal@jasperindia.com>
2015-09-05 01:12:39 +0530

    search custom redirection fix

--- web/src/main/java/com/snapdeal/pears/whatsapp3c/service/MessageService.java
+++ web/src/main/java/com/snapdeal/pears/whatsapp3c/service/MessageService.java
@@ -26,154 +26,154 @@ import com.snapdeal.pears.whatsapp3c.requestresponse.WatsAppMessage;
  */
 public class MessageService {
 
-    public static final ObjectMapper mapper = new ObjectMapper();
-    
-    
-    private Map<Long, List<WatsAppMessage>> messagesHolder = new HashMap<Long, List<WatsAppMessage>>();
+    public static final ObjectMapper        mapper               = new ObjectMapper();
 
-	private Vector<Long> onGoingConversations = new Vector<Long>();
+    private Map<Long, List<WatsAppMessage>> messagesHolder       = new HashMap<Long, List<WatsAppMessage>>();
 
-	public ConversationList prepareConversationList() {
-		ConversationList conversationList = new ConversationList();
-		Set<Long> phoneNumbers = messagesHolder.keySet();
-		Iterator<Long> coversations = onGoingConversations.iterator();
-		while (coversations.hasNext()) {
-			long number = coversations.next();
-			phoneNumbers.remove(number);
-		}
+    private Vector<Long>                    onGoingConversations = new Vector<Long>();
 
-		for (Long number : phoneNumbers) {
-			WatsAppMessage message = getFirstUnreadMessage(number);
-			Conversation conversation = new Conversation(number, message.getMessage(), message.getProducerTs());
-			conversationList.addConversation(conversation);
-		}
-		return conversationList;
+    public ConversationList prepareConversationList() {
+        ConversationList conversationList = new ConversationList();
+        Set<Long> phoneNumbers = messagesHolder.keySet();
+        Iterator<Long> coversations = onGoingConversations.iterator();
+        while (coversations.hasNext()) {
+            long number = coversations.next();
+            phoneNumbers.remove(number);
+        }
 
-	}
+        for (Long number : phoneNumbers) {
+            WatsAppMessage message = getFirstUnreadMessage(number);
+            Conversation conversation = new Conversation(number, message.getMessage(), message.getProducerTs());
+            conversationList.addConversation(conversation);
+        }
+        return conversationList;
 
-	public WatsAppMessage getFirstUnreadMessage(long phoneNumber) {
-		List<WatsAppMessage> watsAppMessages = messagesHolder.get(phoneNumber);
-		WatsAppMessage unReadMessage = null;
+    }
 
-		for (int i = 0; i < watsAppMessages.size(); i++) {
-			WatsAppMessage message = watsAppMessages.get(i);
-			if (!message.isRead()) {
-				unReadMessage = message;
-				break;
-			}
-		}
-		return unReadMessage;
-	}
+    public WatsAppMessage getFirstUnreadMessage(long phoneNumber) {
+        List<WatsAppMessage> watsAppMessages = messagesHolder.get(phoneNumber);
+        WatsAppMessage unReadMessage = null;
 
-	public boolean lockConversation(long phoneNumber) {
-		if (onGoingConversations.contains(phoneNumber)) {
-			return false;
-		} else {
-			onGoingConversations.add(phoneNumber);
-			return true;
-		}
-	}
+        for (int i = 0; i < watsAppMessages.size(); i++) {
+            WatsAppMessage message = watsAppMessages.get(i);
+            if (!message.isRead()) {
+                unReadMessage = message;
+                break;
+            }
+        }
+        return unReadMessage;
+    }
 
-	public void unlockConversation(long phoneNumber) {
-		onGoingConversations.remove(phoneNumber);
-	}
+    public boolean lockConversation(long phoneNumber) {
+        if (onGoingConversations.contains(phoneNumber)) {
+            return false;
+        } else {
+            onGoingConversations.add(phoneNumber);
+            return true;
+        }
+    }
 
-	public Long postMessage(long phoneNumber, String message, String messageId, boolean sender) {
-		List<WatsAppMessage> watsAppMessages = messagesHolder.get(phoneNumber);
-		WatsAppMessage watsAppmessage = null;
-		int id =0;
-		if (watsAppMessages == null) {
-			watsAppMessages = new ArrayList<WatsAppMessage>();
-			watsAppmessage = getWatsAPPMessage(0, message, messageId, sender);
+    public void unlockConversation(long phoneNumber) {
+        onGoingConversations.remove(phoneNumber);
+    }
 
-		} else {
+    public Long postMessage(long phoneNumber, String message, String messageId, boolean sender) {
+        List<WatsAppMessage> watsAppMessages = messagesHolder.get(phoneNumber);
+        WatsAppMessage watsAppmessage = null;
+        int id = 0;
+        if (watsAppMessages == null) {
+            watsAppMessages = new ArrayList<WatsAppMessage>();
+            watsAppmessage = getWatsAPPMessage(0, message, messageId, sender);
 
-			id = watsAppMessages.size();
-			watsAppmessage = getWatsAPPMessage(id, message, messageId, sender);
-		}
-		watsAppMessages.add(watsAppmessage);
-		Collections.sort(watsAppMessages);
-		messagesHolder.put(phoneNumber, watsAppMessages);
-		return (long)id;
-	}
+        } else {
 
-	/**
-	 * This will return the list of messages
-	 * 
-	 * @param phoneNumber
-	 * @param startOffset
-	 * @param endOffset
-	 * @return
-	 */
-	public List<WatsAppMessage> getMessages(long phoneNumber, int startOffset, int endOffset) {
-		List<WatsAppMessage> messages = messagesHolder.get(phoneNumber);
-		int size = messages.size();
-		if( endOffset > size){
-			endOffset = size;
-		}
-		List<WatsAppMessage> returnMessages = new ArrayList<WatsAppMessage>();
-		for (int i = startOffset; i < endOffset; i++) {
-			WatsAppMessage watsAappMessage = messages.get(i);
-			watsAappMessage.setRead(true);
-			returnMessages.add(watsAappMessage);
-		}
-		return returnMessages;
-	}
+            id = watsAppMessages.size();
+            watsAppmessage = getWatsAPPMessage(id, message, messageId, sender);
+        }
+        watsAppMessages.add(watsAppmessage);
+        Collections.sort(watsAppMessages);
+        messagesHolder.put(phoneNumber, watsAppMessages);
+        return (long) id;
+    }
 
-	private WatsAppMessage getWatsAPPMessage(int id, String message, String messageId, boolean sender) {
-		WatsAppMessage watsapp_message = new WatsAppMessage(message, messageId, id, sender);
-		return watsapp_message;
+    /**
+     * This will return the list of messages
+     *
+     * @param phoneNumber
+     * @param startOffset
+     * @param endOffset
+     * @return
+     */
+    public List<WatsAppMessage> getMessages(long phoneNumber, int startOffset, int endOffset) {
+        List<WatsAppMessage> messages = messagesHolder.get(phoneNumber);
+        int size = messages.size();
+        if (endOffset > size) {
+            endOffset = size;
+        }
+        List<WatsAppMessage> returnMessages = new ArrayList<WatsAppMessage>();
+        for (int i = startOffset; i < endOffset; i++) {
+            WatsAppMessage watsAappMessage = messages.get(i);
+            watsAappMessage.setRead(true);
+            returnMessages.add(watsAappMessage);
+        }
+        return returnMessages;
+    }
 
-	}
+    private WatsAppMessage getWatsAPPMessage(int id, String message, String messageId, boolean sender) {
+        WatsAppMessage watsapp_message = new WatsAppMessage(message, messageId, id, sender);
+        return watsapp_message;
 
-	public Map<Long, List<WatsAppMessage>> getMessagesHolder() {
-		return messagesHolder;
-	}
+    }
 
-	public void setMessagesHolder(Map<Long, List<WatsAppMessage>> messagesHolder) {
-		this.messagesHolder = messagesHolder;
-	}
+    public Map<Long, List<WatsAppMessage>> getMessagesHolder() {
+        return messagesHolder;
+    }
 
+    public void setMessagesHolder(Map<Long, List<WatsAppMessage>> messagesHolder) {
+        this.messagesHolder = messagesHolder;
+    }
 
     public static void main(String[] args) throws IOException {
         System.out.println("Search");
-        System.out.println(getSearchResults("samsung galaxy s duos").toString());
-        System.out.println("Order Status");
+        System.out.println(getSearchResults("samsung galaxy duos 2").toString());
+        /*System.out.println("Order Status");
         System.out.println(getOrderStatus("8274798095", "lokesh.chhaparwal@jasperindia.com").toString());
         System.out.println("Trending Products");
-        System.out.println(getTrendingProducts().toString());
+        System.out.println(getTrendingProducts().toString());*/
     }
 
     public static List<ReplyMedia> getSearchResults(String keyword) throws IOException {
         List<ReplyMedia> rms = new ArrayList<ReplyMedia>();
         Document doc = Jsoup.connect("http://www.snapdeal.com/search?keyword=" + keyword + "&noOfResults=4").get();
         Elements els = doc.getElementsByClass("productWrapper");
-        for (Element el : els) {
+        int index = 0;
+        while (index < 4 && index < els.size()) {
+            Element el = els.get(index);
             StringBuilder sb = new StringBuilder();
             ReplyMedia rm = new ReplyMedia();
             Elements els1 = el.getElementsByClass("product-price");
             Elements els2 = el.getElementsByClass("product-title");
             Elements els3 = el.getElementsByClass("hoverProductImage");
+            String imageUrl = "";
             String url = "";
             String name = "";
             String price = "";
-            String imageUrl = "";
-            for (Element el2 : els2) {
-                Elements childEls2 = el2.getElementsByAttribute("href");
-                url = childEls2.get(0).attr("href");
-                name = childEls2.get(0).childNodes().get(0).toString();
+            if (els3.size() == 0) {
+                imageUrl = el.getElementsByClass("gridViewImage").get(0).attr("src");
+            } else {
+                imageUrl = els3.get(0).getElementsByAttribute("href").get(0).childNodes().get(1).attributes().get("src");
             }
-            for (Element el3 : els3) {
-                Elements childEls3 = el3.getElementsByAttribute("href");
-                imageUrl = childEls3.get(0).childNodes().get(1).attributes().get("src");
-            }
-            for (Element el1 : els1) {
-                price = el1.getElementsByAttribute("id").get(0).childNodes().get(0).toString();
+            url = els2.get(0).getElementsByAttribute("href").get(0).attr("href");
+            name = els2.get(0).getElementsByAttribute("href").get(0).childNodes().get(0).toString();
+            price = els1.get(0).getElementsByAttribute("id").get(0).childNodes().get(0).toString();
+            if (price.contains("span")) {
+                price = els1.get(0).childNodes().get(1).childNodes().get(1).childNodes().get(0).toString();
             }
             rm.setPath(imageUrl);
             sb.append(name + "\n" + price + "\n" + url);
             rm.setCaption(sb.toString());
             rms.add(rm);
+            index++;
         }
         return rms;
     }
@@ -236,8 +236,5 @@ public class MessageService {
         }
         return rms;
     }
-    
-    
-    
-    
+
 }
